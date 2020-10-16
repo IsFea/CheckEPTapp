2eb58e13a353194b47929b81420749a55317afa7
isFea <kovalevskiy2011@gmail.com>
2020-10-16 23:19:05 +0300

    task list example

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
+ 
 export default function App() {
+ 
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+    <View style= {styles.container}>
+      <View style = {styles.inputContainer}>
+        <TextInput
+          placeholder = "Task List"
+          style = {styles.input}
+        />
+        <Button title = "+"/> 
+      </View>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input :{
+    borderColor:"black", 
+    borderWidth:1 , 
+    padding :20,
+  },
+  inputContainer :{
+    flexDirection :'row', 
+    justifyContent :'space-between', 
+    alignContent:'center',
+    bottom:20
   },
 });
