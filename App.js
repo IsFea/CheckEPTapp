1230657f67e2f3668959ed53a9b46841043bba69
isFea <kovalevskiy2011@gmail.com>
2020-10-17 00:41:52 +0300

    upd разбор реакта

--- App.js
+++ App.js
@@ -1,23 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React , {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import {
+  AppRegistry
+  , Dimensions
+  , StyleSheet
+  , Text
+  , View
+  , TextInput
+  , TouchableHighlight
+  , Button
+} from 'react-native';
+import Camera from 'react-native-camera';
 
- 
+
 export default function App() {
- 
+  const [newTask, setNewTask] = useState('');
+  const taskInputHandler = (enteredText) => {
+    setNewTask(enteredText);
+  };
+
   return (
-    <View style= {styles.container}>
-      <View style = {styles.inputContainer}>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Text>Task Lists4</Text>
+      </View>
+
+      <View style={styles.inputContainer}>
         <TextInput
-          placeholder = "Task List"
-          style = {styles.input}
+          placeholder="Task List"
+          style={styles.input}
+          onChangeText={taskInputHandler}
+          value={newTask}
         />
-        <Button title = "+"/> 
+        <Button title="+" />
       </View>
     </View>
   );
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -25,15 +45,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input :{
-    borderColor:"black", 
-    borderWidth:1 , 
-    padding :20,
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 20,
   },
-  inputContainer :{
-    flexDirection :'row', 
-    justifyContent :'space-between', 
-    alignContent:'center',
-    bottom:20
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    bottom: 20
   },
 });
